7bc2fd823cd146a4cea66eeee027f5e2dfed1956
Wendy Fgigueroa <camomila444@gmail>
2021-03-21 22:02:33 -0400

    working on service

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,7 @@
 const FILES_TO_CACHE = [
     "/",
     "/db.js",
+    "/index.html",
     "/index.js",
     "/manifest.webmanifest",
     "/styles.css",
@@ -10,45 +11,44 @@ const FILES_TO_CACHE = [
   
   ];
   
-  const STATIC_CACHE = "static-cache-v1";
-  const RUNTIME_CACHE = "runtime-cache";
+  const CACHE_NAME = "static-cache-v2";
+  const DATA_CACHE_NAME = "data-cache-v1";
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches
-      .open(STATIC_CACHE)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
-      .then(() => self.skipWaiting())
-  );
+      .open(CACHE_NAME)
+      .then(cache => {
+        console.log("Offline information pre-cached successfully");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+  )
+   .then(() => self.skipWaiting())
 });
 
 // The activate handler takes care of cleaning up old caches.
 self.addEventListener("activate", event => {
-  const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
-  event.waitUntil(
-    caches
-      .keys()
-      .then(cacheNames => {
-        // return array of cache names that are old to delete
-        return cacheNames.filter(
-          cacheName => !currentCaches.includes(cacheName)
-        );
+event.waitUntil(
+catches.key()
+.then(keyList => {
+    // return array of cache names that are old to delete
+      return Promise
+       .all(keyList.map(key => {
+       if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+       console.log("Deleting previous cached information", key);
+       return caches.delete(key);
+       }
       })
-      .then(cachesToDelete => {
-        return Promise.all(
-          cachesToDelete.map(cacheToDelete => {
-            return caches.delete(cacheToDelete);
-          })
-        );
-      })
-      .then(() => self.clients.claim())
-  );
-});
+    )
+  })     
+  )  
+  .then(() => self.clients.claim()) 
+  });
 
-self.addEventListener("fetch", event => {
+  // fetch
+  self.addEventListener("fetch", event => {
   // non GET requests are not cached and requests to other origins are not cached
-  if (
-    event.request.method !== "GET" ||
+  if (event.request.method !== "GET" ||
     !event.request.url.startsWith(self.location.origin)
   ) {
     event.respondWith(fetch(event.request));
@@ -71,6 +71,8 @@ self.addEventListener("fetch", event => {
     return;
   }
 
+
+  
   // use cache first for all other requests for performance
   event.respondWith(
     caches.match(event.request).then(cachedResponse => {
